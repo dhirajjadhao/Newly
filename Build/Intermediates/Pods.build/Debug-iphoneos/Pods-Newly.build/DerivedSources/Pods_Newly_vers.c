990c1428aa72fc3113f62e896384712e800d1f06
Dhiraj Rajendra Jadhao <dhiraj@tagrem.com>
2016-11-18 14:39:57 +0530

    initial Commit

---
+++ Build/Intermediates/Pods.build/Debug-iphoneos/Pods-Newly.build/DerivedSources/Pods_Newly_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_NewlyVersionString[];
+ extern const double Pods_NewlyVersionNumber;
+
+ const unsigned char Pods_NewlyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Newly  PROJECT:Pods-1" "\n";
+ const double Pods_NewlyVersionNumber __attribute__ ((used)) = (double)1.;
