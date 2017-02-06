300dabd18df14d08190c4a8000896857d349dc45
Dhiraj Jadhao <dhirajjadhao@gmail.com>
2017-02-06 19:43:54 +0530

    update

---
+++ Build/Intermediates/Pods.build/Debug-iphoneos/Pods-Newly.build/Objects-normal/arm64/Pods_Newly_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/prajaktakulkarni/Documents/My\ Pods/Newly/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-Newly.build/DerivedSources/Pods_Newly_vers.c
---
+++ Build/Products/Debug-iphoneos/Newly.framework/Headers/Newly.h
@@ -0,0 +1,19 @@
+//
+//  Newly.h
+//  Newly
+//
+//  Created by Dhiraj Jadhao on 18/11/16.
+//  Copyright © 2016 Dhiraj Jadhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Newly.
+FOUNDATION_EXPORT double NewlyVersionNumber;
+
+//! Project version string for Newly.
+FOUNDATION_EXPORT const unsigned char NewlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Newly/PublicHeader.h>
+
+
